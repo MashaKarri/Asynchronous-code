856506cd9972b3d529ecdf6fd853d287caa99c38
Mariya Dovgalyuk <162305105+MashaKarri@users.noreply.github.com>
2025-11-17 00:39:15 +0200

    Asynchronous-code-3

--- src/index.js
+++ src/index.js
@@ -31,7 +31,6 @@
 //   }
 // }, 1000);
 
-
 // У наведеному нижче коді заплановано виклик setTimeout,
 // потім виконується важкий розрахунок, на виконання якого потрібно більше 100 мс.
 // Коли запуститься запланована функція?
@@ -50,7 +49,7 @@
 // Виводьте значення лічильника на сторінці.
 const startBtn = document.querySelector(".startBtn");
 const endBtn = document.querySelector(".endBtn");
-const container = document.querySelector(".container");
+const counterBlock = document.querySelector(".counter-block");
 
 let timer;
 let total = 0;
@@ -59,7 +58,7 @@ startBtn.addEventListener("click", () => {
   total = 0;
   timer = setInterval(() => {
     total += 1;
-    container.innerHTML = total;
+    counterBlock.innerHTML = total;
   }, 1000);
 });
 
@@ -70,21 +69,105 @@ endBtn.addEventListener("click", () => {
 
 // Завдання "Зміна кольору": Створіть блок, який змінює свій фоновий колір кожні 3 секунди.
 // Використовуйте setInterval для зміни кольору.
-const block = document.querySelector(".block");
+const colorBlock = document.querySelector(".color-block");
 
 setInterval(() => {
-  const color = `rgb(${Math.floor(Math.random()*256)}, ${Math.floor(Math.random()*256)}, ${Math.floor(Math.random()*256)})`;
-  block.style.backgroundColor = color;
+  const color = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(
+    Math.random() * 256
+  )}, ${Math.floor(Math.random() * 256)})`;
+  colorBlock.style.backgroundColor = color;
 }, 3000);
 
 
 // Завдання "Затримка повідомлення": Створіть функцію, яка приймає текстове повідомлення та
 // затримує його виведення на сторінку на певний час, використовуючи setTimeout.
-const area = document.querySelector(".area");
+const textBlock = document.querySelector(".text-block");
 function textMessage(message) {
   setTimeout(() => {
-    area.innerHTML = message;
+    textBlock.innerHTML = message;
   }, 4000);
 }
 
 textMessage("Hello Everyone!");
+
+
+// Напишіть функцію printNumbers(from, to) яка виводить число кожну секунду,
+// починаючи від from і закінчуючи to.
+// Зробіть два варіанти рішення.
+const numberBlock = document.querySelector(".number-block");
+
+// 1. Використовуючи setInterval.
+// function printNumbers(from, to) {
+//   let current = from;
+//   const id = setInterval(() => {
+//     numberBlock.innerHTML = current;
+//     if (current === to) {
+//       clearInterval(id);
+//     }
+//     current += 1;
+//   }, 1000);
+// }
+// printNumbers(5, 10);
+
+// 2. Використовуючи вкладений setTimeout.
+function printNumbers(from, to) {
+  numberBlock.innerHTML = from;
+  if (from < to) {
+    setTimeout(() => {
+      printNumbers(from + 1, to);
+    }, 1000);
+  }
+}
+
+printNumbers(10, 20);
+
+
+// Завдання "Зміна стилів": Створіть блок, який змінює свої стилі
+// (наприклад, фоновий колір, розмір шрифту) через певний час.
+// Використовуйте setTimeout для зміни стилів блоку.
+const styleBlock = document.querySelector(".style-block");
+
+setTimeout(() => {
+  const backgroundColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(
+    Math.random() * 256
+  )}, ${Math.floor(Math.random() * 256)})`;
+  styleBlock.style.backgroundColor = backgroundColor;
+
+  const fontSize = Math.floor(Math.random() * (32 - 16 + 1)) + 16;
+  styleBlock.style.fontSize = fontSize + "px";
+}, 5000);
+
+
+// Завдання "Переключення слайдів": Створіть галерею слайдів,
+// яка автоматично перемикається на наступний слайд через певний інтервал, використовуючи setTimeout.
+const slides = document.querySelectorAll(".slide");
+let index = 0;
+slides[0].classList.add("active");
+
+function changeSlide() {
+  slides[index].classList.remove("active");
+  index += 1;
+
+  if (index === slides.length) {
+    index = 0;
+  }
+
+  slides[index].classList.add("active");
+
+  setTimeout(changeSlide, 3500);
+}
+
+setTimeout(changeSlide, 3500);
+
+
+// Завдання "Зміна розміру": Створіть блок, який змінює свій розмір кожні 4 секунди.
+// Використовуйте setInterval для зміни ширини та висоти блоку.
+const sizeBlock = document.querySelector(".size-block");
+
+setInterval(() => {
+  const width = Math.floor(Math.random() * (500 - 100 + 1)) + 100;
+  sizeBlock.style.width = width + "px";
+
+  const height = Math.floor(Math.random() * (450 - 100 + 1)) + 100;
+  sizeBlock.style.height = height + "px";
+}, 4000);
