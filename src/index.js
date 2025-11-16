34954cd7e77bd14da2bca54222c99e2756729493
Mariya Dovgalyuk <162305105+MashaKarri@users.noreply.github.com>
2025-11-16 20:59:31 +0200

    Asynchronous-code-2

--- src/index.js
+++ src/index.js
@@ -1,35 +1,35 @@
 // setTimeout() - виконує код ОДИН раз через вказаний час
-setTimeout(
-  (a) => {
-    console.log("Test setTimeout", a);
-    console.log(a);
-  },
-  500,
-  "argument"
-);
+// setTimeout(
+//   (a) => {
+//     console.log("Test setTimeout", a);
+//     console.log(a);
+//   },
+//   500,
+//   "argument"
+// );
 
-const x = 2;
-console.log(x);
-const y = 5;
-console.log(y);
+// const x = 2;
+// console.log(x);
+// const y = 5;
+// console.log(y);
 
-setTimeout(() => {
-  console.log("Другий setTimeout");
-}, 2500);
+// setTimeout(() => {
+//   console.log("Другий setTimeout");
+// }, 2500);
 
 // В КОДІ ПЕРШИМ ЗАВЖДИ ВИКОНУЄТЬСЯ СИНХРОННИЙ КОД, А ПІСЛЯ СИНХРОННОГО - АСИНХРОННИЙ
 // ЯКЩО Є КІЛЬКА АСИНХРОННИХ ОПЕРАЦІЙ, ТО ВОНИ ВИКОНАЮТЬСЯ В ПОРЯДКУ "ХТО ПЕРШИЙ СПРАЦЮВАВ"
 
 // setInterval() - повторює виконання коду через вказаний час
-let count = 0;
-const id = setInterval(() => {
-  count += 1;
-  console.log(`Пройшло: ${count}`);
+// let count = 0;
+// const id = setInterval(() => {
+//   count += 1;
+//   console.log(`Пройшло: ${count}`);
 
-  if (count === 5) {
-    clearInterval(id);
-  }
-}, 1000);
+//   if (count === 5) {
+//     clearInterval(id);
+//   }
+// }, 1000);
 
 
 // У наведеному нижче коді заплановано виклик setTimeout,
@@ -44,3 +44,47 @@ const id = setInterval(() => {
 //   num += 1;
 // }
 // Відповідь: після циклу. Alert виведе результат циклу (100000000)
+
+
+// Завдання "Лічильник": Створіть лічильник, який збільшується на одиницю кожну секунду.
+// Виводьте значення лічильника на сторінці.
+const startBtn = document.querySelector(".startBtn");
+const endBtn = document.querySelector(".endBtn");
+const container = document.querySelector(".container");
+
+let timer;
+let total = 0;
+
+startBtn.addEventListener("click", () => {
+  total = 0;
+  timer = setInterval(() => {
+    total += 1;
+    container.innerHTML = total;
+  }, 1000);
+});
+
+endBtn.addEventListener("click", () => {
+  clearInterval(timer);
+});
+
+
+// Завдання "Зміна кольору": Створіть блок, який змінює свій фоновий колір кожні 3 секунди.
+// Використовуйте setInterval для зміни кольору.
+const block = document.querySelector(".block");
+
+setInterval(() => {
+  const color = `rgb(${Math.floor(Math.random()*256)}, ${Math.floor(Math.random()*256)}, ${Math.floor(Math.random()*256)})`;
+  block.style.backgroundColor = color;
+}, 3000);
+
+
+// Завдання "Затримка повідомлення": Створіть функцію, яка приймає текстове повідомлення та
+// затримує його виведення на сторінку на певний час, використовуючи setTimeout.
+const area = document.querySelector(".area");
+function textMessage(message) {
+  setTimeout(() => {
+    area.innerHTML = message;
+  }, 4000);
+}
+
+textMessage("Hello Everyone!");
