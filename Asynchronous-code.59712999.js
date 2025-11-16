220450dfd56302ab52edc8bae7caae7cb78139d4
Mariya Dovgalyuk <162305105+MashaKarri@users.noreply.github.com>
2025-11-16 21:00:08 +0200

    Updates

--- Asynchronous-code.59712999.js
+++ Asynchronous-code.59712999.js
@@ -207,15 +207,15 @@
       });
     }
   }
-})({"5j6Kf":[function(require,module,exports,__globalThis) {
+})({"9JJ5D":[function(require,module,exports,__globalThis) {
 var global = arguments[3];
 var HMR_HOST = null;
 var HMR_PORT = null;
 var HMR_SERVER_PORT = 1234;
 var HMR_SECURE = false;
-var HMR_ENV_HASH = "439701173a9199ea";
+var HMR_ENV_HASH = "d6ea1d42532a7575";
 var HMR_USE_SSE = false;
-module.bundle.HMR_BUNDLE_ID = "d68ad56631b563d9";
+module.bundle.HMR_BUNDLE_ID = "7055c94b59712999";
 "use strict";
 /* global HMR_HOST, HMR_PORT, HMR_SERVER_PORT, HMR_ENV_HASH, HMR_SECURE, HMR_USE_SSE, chrome, browser, __parcel__import__, __parcel__importScripts__, ServiceWorkerGlobalScope */ /*::
 import type {
@@ -713,78 +713,8 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
     }
 }
 
-},{}],"a0t4e":[function(require,module,exports,__globalThis) {
-// setTimeout() - виконує код ОДИН раз через вказаний час
-// setTimeout(
-//   (a) => {
-//     console.log("Test setTimeout", a);
-//     console.log(a);
-//   },
-//   500,
-//   "argument"
-// );
-// const x = 2;
-// console.log(x);
-// const y = 5;
-// console.log(y);
-// setTimeout(() => {
-//   console.log("Другий setTimeout");
-// }, 2500);
-// В КОДІ ПЕРШИМ ЗАВЖДИ ВИКОНУЄТЬСЯ СИНХРОННИЙ КОД, А ПІСЛЯ СИНХРОННОГО - АСИНХРОННИЙ
-// ЯКЩО Є КІЛЬКА АСИНХРОННИХ ОПЕРАЦІЙ, ТО ВОНИ ВИКОНАЮТЬСЯ В ПОРЯДКУ "ХТО ПЕРШИЙ СПРАЦЮВАВ"
-// setInterval() - повторює виконання коду через вказаний час
-// let count = 0;
-// const id = setInterval(() => {
-//   count += 1;
-//   console.log(`Пройшло: ${count}`);
-//   if (count === 5) {
-//     clearInterval(id);
-//   }
-// }, 1000);
-// У наведеному нижче коді заплановано виклик setTimeout,
-// потім виконується важкий розрахунок, на виконання якого потрібно більше 100 мс.
-// Коли запуститься запланована функція?
-// Що виведе alert?
-// let num = 0;
-// setTimeout(() => alert(num), 100); // ?
-// for (let i = 0; i < 100000000; i += 1) {
-//   num += 1;
-// }
-// Відповідь: після циклу. Alert виведе результат циклу (100000000)
-// Завдання "Лічильник": Створіть лічильник, який збільшується на одиницю кожну секунду.
-// Виводьте значення лічильника на сторінці.
-const startBtn = document.querySelector(".startBtn");
-const endBtn = document.querySelector(".endBtn");
-const container = document.querySelector(".container");
-let timer;
-let total = 0;
-startBtn.addEventListener("click", ()=>{
-    total = 0;
-    timer = setInterval(()=>{
-        total += 1;
-        container.innerHTML = total;
-    }, 1000);
-});
-endBtn.addEventListener("click", ()=>{
-    clearInterval(timer);
-});
-// Завдання "Зміна кольору": Створіть блок, який змінює свій фоновий колір кожні 3 секунди.
-// Використовуйте setInterval для зміни кольору.
-const block = document.querySelector(".block");
-setInterval(()=>{
-    const color = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
-    block.style.backgroundColor = color;
-}, 3000);
-// Завдання "Затримка повідомлення": Створіть функцію, яка приймає текстове повідомлення та
-// затримує його виведення на сторінку на певний час, використовуючи setTimeout.
-const area = document.querySelector(".area");
-function textMessage(message) {
-    setTimeout(()=>{
-        area.innerHTML = message;
-    }, 4000);
-}
-textMessage("Hello Everyone!");
+},{}],"4M6V8":[function(require,module,exports,__globalThis) {
 
-},{}]},["5j6Kf","a0t4e"], "a0t4e", "parcelRequiree4b1", {})
+},{}]},["9JJ5D","4M6V8"], "4M6V8", "parcelRequiree4b1", {})
 
-//# sourceMappingURL=Asynchronous-code.31b563d9.js.map
+//# sourceMappingURL=Asynchronous-code.59712999.js.map
